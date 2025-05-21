f24d64c10a19f7f76740dc7205412fbe2ac40470
Shema Arsene <aniyonkurushema@gmail.com>
2025-05-21 17:23:16 +0200

    Update default home state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import PaymentSuccess from "./pages/PaymentSuccess"
-import HomePage from "./pages/Homepage"
+import HomePage from "./pages/HomePage"
 
 function App() {
   return (
